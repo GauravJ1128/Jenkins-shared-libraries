97c37aafc3c9f541ae192d5696847d6e68bd8f19
Gaurav Netke <167296105+GauravJ1128@users.noreply.github.com>
2024-10-04 14:48:18 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,6 @@
 def call(String ImageNmae, String ImageTag, String DockerHubUser){
-  withCredentials([usernamePassword(credentialsId: "DockerHub-cred",
-                passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
-                sh "docker push ${DockerHubUser}/${ImageNmae}:${ImageTag}"
+  withCredentials([usernamePassword(credentialsId: "DockerHub-cred", passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+                sh "docker login -u ${DockerHubUser} -p ${DockerHubPass}"
                 }
+                sh "docker push ${DockerHubUser}/${ImageNmae}:${ImageTag}"
 }
